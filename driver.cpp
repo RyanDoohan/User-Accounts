cb479c23d0ee7a7b0d8ed13204c2fcdb756d0e8d
RyanDoohan <doohanryan@gmail.com>
2021-07-24 15:26:51 -0400

    Implement Signin, user count, and display list of users

--- driver.cpp
+++ driver.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 int print_welcome_message();
 void create_account();
+void signin_account();
+void print_current_users();
+int count_users();
 
 int main() {
     int users_choice_int;
@@ -18,6 +21,11 @@ int main() {
             break;
         }
         case 2: {
+            signin_account();
+            break;
+        }
+        case 3: {
+            print_current_users();
             break;
         }
         break;
@@ -32,13 +40,14 @@ int print_welcome_message() {
     cout << "Demo Account Creation Software\n";
     cout << "=-==============================-=\n\n";
     cout << "1.) Create a New Account\n";
-    cout << "2.) Sign-in to an Existing Account\n\n";
+    cout << "2.) Sign-in to an Existing Account\n";
+    cout << "3.) List current users\n";
     cout << "Enter here: ";
 
     getline(cin, users_choice);
 
-    while(users_choice != "1" && users_choice != "2") { // Input validation check
-        cout << "You have entered an invalid input. Accepted inputs are '1' or '2'. Please enter only accepted inputs to proceed.\n";
+    while(users_choice != "1" && users_choice != "2" && users_choice != "3") { // Input validation check
+        cout << "You have entered an invalid input. Accepted inputs are '1' or '2' or '3'. Please enter only accepted inputs to proceed.\n";
         cout << "Enter here: ";
         
         getline(cin, users_choice);
@@ -83,4 +92,76 @@ void create_account() {
         accountsInfoWrite.close();
     }
     else cout << "There was an error opening 'accounts_info.txt'. Please ensure this file exists and then restart the program.\n";
+}
+
+void signin_account() {
+    string username, password, readFromFile;
+
+    ifstream accountsInfoRead ("accounts_info.txt");
+
+    cout << "\n\nSign-in to an existing account\n";
+    cout << "=-==============================-=\n\n";
+    cout << "Enter Username: ";
+
+    getline(cin, username);
+
+    while(username.compare("1") != 0) {
+        while(accountsInfoRead >> readFromFile) {
+            if(readFromFile.compare(username) == 0) {
+                cout << "Enter Password: ";
+                getline(cin, password);
+                accountsInfoRead >> readFromFile;
+                while(readFromFile.compare(password) != 0) {
+                        cout << "Password entered was incorrect.\n";
+                        cout << "Enter password here: ";
+
+                        getline(cin, password);
+                }
+                cout << "Succesfully signed into account!\n";
+                return;
+            }
+        }
+
+        cout << "User was not found!\n";
+        cout << "To create a new account enter '1'.\n";
+        cout << "Enter Username: ";
+
+        accountsInfoRead.clear();
+        accountsInfoRead.seekg(0);
+
+        getline(cin, username);
+    }
+
+}
+
+void print_current_users() {
+    string readFromFile, newUserString = "=User=";
+    ifstream accountsInfoRead ("accounts_info.txt");
+
+    cout << "\n\nNumber of current users in the system: " << count_users() << "\n";
+    cout << "=-====================================-=\n";
+
+    while(accountsInfoRead >> readFromFile) {
+        if(readFromFile.compare(newUserString) != 0) {
+            if(readFromFile[0] != '#') {
+             cout << readFromFile;
+             accountsInfoRead >> readFromFile;
+            }
+        }
+        else cout << "\n";
+    }
+}
+
+int count_users() {
+    int num_users = 0;
+
+    string readFromFile, newUserString = "=User=";
+    ifstream accountsInfoRead ("accounts_info.txt");
+
+    while(accountsInfoRead >> readFromFile) {
+        if(readFromFile.compare(newUserString) == 0) {
+            num_users++;
+        }
+    }
+    return num_users;
 }
